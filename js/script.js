88865272bea3a6f75cb9cf9b529ab0566269bb64
Antoine TL <athompsonleduc@gmail.com>
2023-01-24 18:00:55 -0500

    modifications du site

--- js/script.js
+++ js/script.js
@@ -106,3 +106,15 @@
         // divShowData.appendChild(table);     
         
     }
+
+
+
+    $(document).ready(function(){
+        $('.thumbnails a').click(function(e){
+          e.preventDefault();
+    $('.image-gallery img').attr('src',$(this).attr('href'));
+        })
+      });
+
+
+      
